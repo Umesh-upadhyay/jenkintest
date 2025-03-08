030a1464ae8c3345dfd64e39f0e98889f39111df
Umesh Upadhyay <Umesh Upadhyay@DESKTOP-GPDEPC3>
2025-03-08 12:46:32 +0530

    merge

---
+++ src/main/java/um2/um2/Hello.java
@@ -0,0 +1,13 @@
+package um2.um2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String st = "My name is umesh upadhyay";
+		System.out.println(st);
+	}
+
+}
+
